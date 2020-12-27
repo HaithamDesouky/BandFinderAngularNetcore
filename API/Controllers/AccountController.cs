b69783f2df79ed1a4c1d6d7fe7f92d319a85c566
HaithamDesouky <haithamadesouky@gmail.com>
2020-12-27 14:04:48 +0100

    fixes added to mobile

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -57,7 +57,7 @@ namespace API.Controllers
     [HttpPost("login")]
     public async Task<ActionResult<UserDto>> Login(LoginDto loginDto)
     {
-      var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName == loginDto.Username);
+      var user = await _context.Users.Include(p => p.Photos).SingleOrDefaultAsync(x => x.UserName.ToLower() == loginDto.Username.ToLower());
 
       if (user == null) return Unauthorized("Invalid username");
 
